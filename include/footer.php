69e8ccb8aea0f19cebcbc843742b50262fb1a58a
carlo del valle <delvallecarlo@gmail.com>
2018-04-12 16:36:16 +0800

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,75 @@
+<footer id="sub-footer" class="footer-bs">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-4 footer-brand">
+          <a href="index.php"><img src="img/logo3.png" alt="footer logo" class="img-fluid" ></a>
+            <p>© Consolidated Electric Power Asia LTD. 2018</p>
+      </div>
+      <div class="col-md-4 col-sm-6 footer-nav">
+          <h4>Navigation</h4>
+                <ul class="list">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="investor-centre.php">Investor Centre</a></li>
+                    <li><a href="contact-us.php">Contact Us</a></li>
+                </ul>
+       </div>
+
+      <div class="col-sm-6 col-md-4 footer-ns">
+          <h4>Contact Us</h4>
+          <address>
+            <p><strong>Consolidated Electric Power Asia LTD.</strong>
+            <br>Suite 501 & 502, <br>CTC Building No. 1121 Roxas Boulevard,<br>Pasay City, Manila, Philippines.</p>
+          </address>
+          <address>
+            <p><i class="glyphicon glyphicon-earphone"></i>&nbsp;+852 2528 0082</p>
+          </address>
+      </div>
+
+
+    </div>
+  </div>
+</footer>
+
+
+
+
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+
+
+
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="plugins/flexslider/flexslider.css" rel="stylesheet" media="screen" />
+<link href="css/cubeportfolio.min.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+
+
+<!-- <link href="css/bootstrap.css" rel="stylesheet" /> -->
+<link href="plugins/flexslider/flexslider.css" rel="stylesheet" media="screen" />
+
+<link href="css/style.css" rel="stylesheet" />
+
+<link id="bodybg" href="bodybg/bg-c.css" rel="stylesheet" type="text/css" />
+
+<!-- Theme skin -->
+<link id="t-colors" href="skins/yellow.css" rel="stylesheet" />
+
+
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<script src="js/modernizr.custom.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="plugins/flexslider/jquery.flexslider-min.js"></script>
+<script src="plugins/flexslider/flexslider.config.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/stellar.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/uisearch.js"></script>
+<script src="js/jquery.cubeportfolio.min.js"></script>
+<script src="js/google-code-prettify/prettify.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('include/header.php'); ?>
+</head>
+<body>
+		<header>
+			<?php include('include/navigation.php'); ?>
+		</header>
+		<section id="featured" class="bg">
+			<!-- parallax  -->
+			 <div id="parallax1" class="parallax text-light text-center marginbot50" data-stellar-background-ratio="0.5">
+				<div class="container">
+					<div class="header-text">
+							<div class="col-md-8 text-left">
+								<h3 class="light white"><img src="img/logo3.png" class="img-responsive"></h3>
+								<h2 style="color:#FFFFFF">Consolidated Electric Power Asia Limited</h2>
+								<h5 style="text-decoration: none; color:#FFFFFF">PAGBILAO 650MW CCPP O&M PROJECT</h5>
+							</div>
+					</div>
+				</div>
+			</div>
+			<div class="container animated fadeIn">
+				<div class="row">
+					<div class="col-lg-12">
+						 <div class="container">
+							 <div class="row">
+								 <div class="col-sm-8">
+									 <h3>The Company</h3>
+								 	<p><strong style="color:#996633">Consolidated Electric Power Asia Limited (CEPA)</strong> was established for the purpose of assisting developing countries with high electricity demand. Hopewell Holding’s power project investments were grouped under Consolidated Electric Power Asia Limited and listed on the Hong Kong Stock Exchange in late 1993, to develop and operate power generation projects throughout Asia. Originally a subsidiary of Hopewell Holding, Slipform Engineering Ltd. and Tileman Group were then put under CEPA. Senior management and executives of the companies were also transferred to the new companies under CEPA.</p>
+								 	<p><strong style="color:#996633">CEPA</strong> built up to become the largest private power company in Asia at that time, with projects in China, the Philippines, Indonesia and India. CEPA constructs, develops, finances and operates power plants. CEPA provides on-site operation and maintenance services to ensure smooth and efficient operations of power stations and power barges.</p>
+								 	<p><strong style="color:#996633">Mr. Stewart Elliott</strong> is the Managing Director and Chief Executive Officer of the Company and the Group. The principal activities of CEPA and its subsidiary companies are development & operation of power station projects, civil construction and engineering services, project management & treasury investments.</p>
+									<a href="services.php" class="btn btn-theme btn-md marginbot50">Read more <i class="fa fa-angle-right"></i></a>
+								 </div>
+									<div class="col-sm-4">
+										<h3>Contact Us</h3>
+										<Address>
+ 				 							<!-- <i class="glyphicon glyphicon-home"></i> -->
+ 				 							Suite 501 & 502,CTC Building No. 1121 Roxas Boulevard,
+ 				 							<br>CTC Building No. 1121 Roxas Boulevard,
+ 				 							<br>Pasay City, Manila, Philippines.
+											<br><br>
+											<p><i class="glyphicon glyphicon-earphone"></i>&nbsp;(+632) 831 8855 </p>
+										</address>
+									</div>
+									</div>
+								</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="content">
+			<div class="container animated fadeIn">
+				<div class="row">
+					<div class="col-lg-12">
+						 <div class="container">
+							 <div class="row">
+								 <div class="col-sm-8">
+								 	<h4>O&M Contract awarded by:</h4>
+								 	<p>Energy World Power Operations Philippines Inc. (Owner)</p>
+								 	<p>To:</br>Consolidated Electric Power Asia Philippines Corporation (Operator)</p>
+										<img src="img/cepapc.jpg" class="img-responsive">
+									<!-- <a href="about.php" class="btn btn-theme btn-md marginbot50">Read more <i class="fa fa-angle-right"></i></a> -->
+								 </div>
+									</div>
+								</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</body>
+<?php include('include/footer.php'); ?>
+
+</html>
